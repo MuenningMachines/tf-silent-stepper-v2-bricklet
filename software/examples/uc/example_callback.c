e110c0ce6ca18683ba8486609995bd85880dd64a
Matthias Bolte <matthias@tinkerforge.com>
2022-04-26 17:41:04 +0200

    uc: Adapt to bindings changes

--- software/examples/uc/example_callback.c
+++ software/examples/uc/example_callback.c
@@ -1,19 +1,15 @@
 // This example is not self-contained.
-// It requres usage of the example driver specific to your platform.
+// It requires usage of the example driver specific to your platform.
 // See the HAL documentation.
 
-#include "bindings/hal_common.h"
-#include "bindings/bricklet_silent_stepper_v2.h"
+#include "src/bindings/hal_common.h"
+#include "src/bindings/bricklet_silent_stepper_v2.h"
 
-// FIXME: This example is incomplete
+// FIXME: This example is incomplete, missing tf_hal_random(min, max) function
 
-#define UID "XYZ" // Change XYZ to the UID of your Silent Stepper Bricklet 2.0
-
-void check(int rc, const char* msg);
-
+void check(int rc, const char *msg);
 void example_setup(TF_HAL *hal);
 void example_loop(TF_HAL *hal);
-
 
 // Use position reached callback to program random movement
 static void position_reached_handler(TF_SilentStepperV2 *device, int32_t position,
@@ -27,7 +23,7 @@ static TF_SilentStepperV2 ss;
 
 void example_setup(TF_HAL *hal) {
 	// Create device object
-	check(tf_silent_stepper_v2_create(&ss, UID, hal), "create device object");
+	check(tf_silent_stepper_v2_create(&ss, NULL, hal), "create device object");
 
 	// Register position reached callback to function position_reached_handler
 	tf_silent_stepper_v2_register_position_reached_callback(&ss,
--- software/examples/uc/example_configuration.c
+++ software/examples/uc/example_configuration.c
@@ -1,23 +1,19 @@
 // This example is not self-contained.
-// It requres usage of the example driver specific to your platform.
+// It requires usage of the example driver specific to your platform.
 // See the HAL documentation.
 
-#include "bindings/hal_common.h"
-#include "bindings/bricklet_silent_stepper_v2.h"
+#include "src/bindings/hal_common.h"
+#include "src/bindings/bricklet_silent_stepper_v2.h"
 
-#define UID "XYZ" // Change XYZ to the UID of your Silent Stepper Bricklet 2.0
-
-void check(int rc, const char* msg);
-
+void check(int rc, const char *msg);
 void example_setup(TF_HAL *hal);
 void example_loop(TF_HAL *hal);
-
 
 static TF_SilentStepperV2 ss;
 
 void example_setup(TF_HAL *hal) {
 	// Create device object
-	check(tf_silent_stepper_v2_create(&ss, UID, hal), "create device object");
+	check(tf_silent_stepper_v2_create(&ss, NULL, hal), "create device object");
 
 	check(tf_silent_stepper_v2_set_motor_current(&ss,
 	                                             800), "call set_motor_current"); // 800 mA
